6eb8e95bc530103d09e30ae35e0eea05b26809e2
Anna Chen <xqchen91@gmail.com>
2017-09-15 15:06:32 -0400

    feat: write review (cannot retrieve review(s) yet)

--- client/components/WriteReview.jsx
+++ client/components/WriteReview.jsx
@@ -3,56 +3,74 @@ import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { newReview } from '../reducer/review'
 
-function WriteReview(props){
-    
-    return(
-        <div>
-            <h1>Review {props.product.name}</h1>
-            <form onSubmit={props.submitReview}>
-                <label>Reviewing as: {props.reviewerName}</label>
-                <input name='reviewerName' type='hidden' value={props.reviewerName} />
-                <input name='reviewerID' type='hidden' value={props.reviewerID} />
-                <label>Rating:</label>
-                <select name='score'>
-                    {[1,2,3,4,5].map(score => {
-                        return (
-                            <option>{score}</option>
-                        )
-                    })}
-                </select>
-                <label>Review:</label>
-                <input 
-                    name='reviewText'
-                    type='text'
-                    required
-                />
-                <button type='submit'>Submit Review</button>
-            </form>
-        </div>
-    )
+class WriteReview extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            reviewerID: props.reviewerID,
+            reviewerName: props.reviewerName,
+            reviewText: '',
+            score: 0
+        }
+        this.handleSubmit = this.handleSubmit.bind(this)
+        this.handleChange = this.handleChange.bind(this)
+    }
+
+    handleSubmit(event) {
+        event.preventDefault();
+        this.props.submitReview(this.state)
+    }
+
+    handleChange(event) {
+        const field = event.target.name;
+        const content = event.target.value;
+        this.setState({[field]: content})
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>Review {this.props.product.title}</h1>
+                <form onSubmit={this.handleSubmit}>
+                    <label>Reviewing as: {this.props.reviewerName}</label>
+                    <input name='reviewerName' type='hidden' value={this.props.reviewerName} />
+                    <input name='reviewerID' type='hidden' value={this.props.reviewerID} />
+                    <label>Rating:</label>
+                    <select name='score' onChange={this.handleChange}>
+                        {[1, 2, 3, 4, 5].map(score => {
+                            return (
+                                <option>{score}</option>
+                            )
+                        })}
+                    </select>
+                    <label>Review:</label>
+                    <input
+                        name='reviewText'
+                        type='text'
+                        onChange={this.handleChange}
+                        required
+                    />
+                    <button type='submit' disabled={this.state.reviewText.length<10}>Submit Review</button>
+                </form>
+            </div>
+        )
+    }
 }
 
-const mapState = function(state, ownProps){
+const mapState = function (state, ownProps) {
     const prodId = ownProps.match.params.id
     return {
         product: state.products.find(product => product.id === +prodId) || { id: 0, title: '', description: '', price: 0, imURL: '', inventory: 0, category: '' },
         reviewerID: '123', //dummy id
         reviewerName: 'dummy buyer'
-        
+
     }
 }
 
-const mapDispatch = function(dispatch, ownProps){
+const mapDispatch = function (dispatch, ownProps) {
     return {
-        submitReview(event){
-            event.preventDefault();
-            const review = {
-                reviewerID: event.target.reviewerID.value,
-                reviewerName: event.target.reviewerName.value,
-                reviewText: event.target.reviewText.value,
-                score: event.target.score.value
-            }
-            dispatch(newReview(review,ownProps.match.params.id, ownProps.history))
+        submitReview(review) {
+            dispatch(newReview(review, ownProps.match.params.id, ownProps.history))
         }
     }
 }
