10c66b7008d494d876ab482836bd5064ee96469c
ebeastlake <ebeastlake@gmail.com>
2017-09-17 00:31:26 -0400

    chore: need to look for select on another branch

--- client/components/SingleProduct.jsx
+++ client/components/SingleProduct.jsx
@@ -9,6 +9,83 @@ function SingleProduct(props) {
 	const product = props.currentProduct;
 	
 	return (
+		<div className="container">
+
+		<div className="row">
+		<div className="col-md-12">
+            <div className="product-content-right">
+                        {/*<div class="product-breadcroumb">
+                            <a href="">Home</a>
+                            <a href="">Category Name</a>
+                            <a href="">Sony Smart TV - 2015</a>
+                        </div>*/}
+
+        <div className="col-sm-3">
+          <div className="product-images">
+            <div className="product-main-img">
+              <img src="img/product-2.jpg" alt />
+            </div>
+          </div>
+        </div>
+        <div className="col-sm-9">
+          <div className="product-inner">
+            <h2 className="product-name">{product.title}</h2>
+            <div className="product-inner-price">
+              <ins>${Number.parseInt(product.price).toFixed(2)}</ins>
+            </div>    
+            <form action className="cart">
+              <div className="quantity">
+                <input type="number" size={4} className="input-text qty text" title="Qty" defaultValue={1} name="quantity" min={1} step={1} />
+              </div>
+             
+              <button className="add_to_cart_button" type="submit">Add to cart</button>
+            </form>   
+            <div className="product-inner-category">
+              <p>Category: <a href>Summer</a>. Tags: <a href>awesome</a>, <a href>best</a>, <a href>sale</a>, <a href>shoes</a>. </p>
+            </div>
+          </div>
+        </div>
+      </div>
+      </div>
+      </div>
+
+      <div className="row">
+      	<div className="col-md-12">
+      		<div role="tabpanel">
+              <ul className="product-tab" role="tablist">
+                <li role="presentation" className="active"><a href="#home" aria-controls="home" role="tab" data-toggle="tab">Description</a></li>
+                <li role="presentation"><a href="#profile" aria-controls="profile" role="tab" data-toggle="tab">Reviews</a></li>
+              </ul>
+              <div className="tab-content">
+                <div role="tabpanel" className="tab-pane fade in active" id="home">
+                  <h2>Product Description</h2>  
+                  <p>{product.description}</p>
+                </div>
+                <div role="tabpanel" className="tab-pane fade" id="profile">
+                  <h2>Reviews</h2>
+                  <div className="submit-review">
+                    <p><label htmlFor="name">Name</label> <input name="name" type="text" /></p>
+                    <p><label htmlFor="email">Email</label> <input name="email" type="email" /></p>
+                    <div className="rating-chooser">
+                      <p>Your rating</p>
+                      <div className="rating-wrap-post">
+                        <i className="fa fa-star" />
+                        <i className="fa fa-star" />
+                        <i className="fa fa-star" />
+                        <i className="fa fa-star" />
+                        <i className="fa fa-star" />
+                      </div>
+                    </div>
+                    <p><label htmlFor="review">Your review</label> <textarea name="review" id cols={30} rows={10} defaultValue={""} /></p>
+                    <p><input type="submit" defaultValue="Submit" /></p>
+                  </div>
+                </div>
+              </div>
+            </div>
+      	</div>
+      </div>
+
+
 		<div className="row">
 			<div className="col-lg-6 col-md-6 col-s-12 col-xs-12">
 				<p>{product.title}</p>
@@ -28,6 +105,8 @@ function SingleProduct(props) {
 					<input type="submit" />
 				</form>
 			</div>
+		</div>
+
 		</div>
 	);
 };
