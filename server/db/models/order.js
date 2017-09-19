7ce208b41a8cdba50bba40141f00520a3cdec6a0
ebeastlake <ebeastlake@gmail.com>
2017-09-19 09:53:43 -0400

    chore: need to checkout

--- server/db/models/order.js
+++ server/db/models/order.js
@@ -1,5 +1,6 @@
 const Sequelize = require('sequelize');
 const db = require('../db');
+const Product = db.models.product
 
 const Order = db.define('order', {
 	// orders must belong to a user or a guess session (auth vs. unauth)
@@ -12,6 +13,12 @@ const Order = db.define('order', {
 	status: {
 		type: Sequelize.STRING
 	}
+}, {
+	defaultScope: {
+		include: [
+			{ model: Product }
+		]
+  	}		  	
 });
 
 module.exports = Order;
