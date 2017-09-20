643222eb99f45bd1e6399b356c97114800a1f879
ebeastlake <ebeastlake@gmail.com>
2017-09-19 21:03:49 -0400

    feat: review styling finished

--- client/components/Reviews.jsx
+++ client/components/Reviews.jsx
@@ -21,7 +21,28 @@ class Reviews extends Component {
     }
 
     parseDate(string) {
-        return string.slice(0,10)
+        const stringArray = string.slice(0,10).split("-")
+        const year = stringArray[0]
+        const monthNum = stringArray[1]
+        const day = stringArray[2]
+        const month = (function(month) {  
+          switch(month) {
+            case '01': return 'January'
+            case '02': return 'February'
+            case '03': return 'March'
+            case '04': return 'April'
+            case '05': return 'May'
+            case '06': return 'June'
+            case '07': return 'July'
+            case '08': return 'August'
+            case '09': return 'September'
+            case '10': return 'October'
+            case '11': return 'November'
+            case '12': return 'December'
+            default: return month
+        }})(monthNum)
+        const date = month + ' ' + day + ', ' + year
+        return date
     }
 
     render() {
